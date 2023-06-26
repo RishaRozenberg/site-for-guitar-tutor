5bae1932cb5eecdd05bfbfd1aa6c777d1ac6a5de
RishaRozenberg <teamofdk@gmail.com>
2023-06-27 00:19:13 +0300

    add adaptive max-width: 767px + js in nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.nav-menu'),
-    menuItem = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.nav'),
+        menuItem = document.querySelectorAll('.nav__list'),
+        hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('nav-menu_active');
+        menu.classList.toggle('nav_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('nav-menu_active');
+            menu.classList.toggle('nav_active');
         })
     })
 })
